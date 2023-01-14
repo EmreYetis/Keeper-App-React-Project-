1090a74782a274f0a1ed7c14d441439cde29dd4d
Emre Yetis <emreyetis955@gmail.com>
2023-01-14 13:22:57 +0300

    React Project Start

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+    return(
+    <header>
+        <div>
+            <h1>Keeper</h1>
+        </div>
+    </header>
+    );
+}
+
+
+export default Header;
