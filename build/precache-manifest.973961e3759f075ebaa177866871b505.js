7ffc9a92f41fadabee6f56860df61856f16d69dd
Emre Yetis <emreyetis955@gmail.com>
2024-01-12 13:00:33 +0300

    build

---
+++ build/precache-manifest.973961e3759f075ebaa177866871b505.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d0c35bdfca0f04c2611c7709140f2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9e8b3dc535570f78e04",
+    "url": "/static/js/2.0037794a.chunk.js"
+  },
+  {
+    "revision": "204711c19071becdac92",
+    "url": "/static/js/main.838db30c.chunk.js"
+  },
+  {
+    "revision": "325f4b8f32369198a2c5",
+    "url": "/static/js/runtime-main.4e550677.js"
+  }
+]);
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MdDelete } from "react-icons/md";
 
 function Note(props) {
   function handleClick() {
@@ -9,7 +10,10 @@ function Note(props) {
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
+      <button onClick={handleClick}>
+        {" "}
+        <MdDelete />{" "}
+      </button>
     </div>
   );
 }
